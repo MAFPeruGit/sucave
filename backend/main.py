8c39ae8923743dd2265592806cd4ae5053e672e5
Sebastian Alessandro Roldan Gonzales <sroldan@mafperu.com>
2025-06-10 03:50:57 -0500

    se actualizo cors

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://sucave-web-maf.web.app/"
+        "https://sucave-web-maf.web.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
